b1ff441af1adb6774b57622a8613d557b78c6dd3
Javier Ballester <Javirb26@gmail.com>
2023-12-09 14:24:00 -0500

    added script that populates img depending on user id and task

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,6 +21,24 @@ window.onload = () => {
  const apiUserLink = 'http://localhost:8083/api/users';
  const apiTodoIdLink = 'http://localhost:8083/api/todos/byuser'
 
+ const userImages = {
+    1: 'assets/images/study_todo.png',
+    2: 'assets/images/temu_shrek.png',
+    3: 'assets/images/v_bucks.png',
+    4: 'assets/images/dog_bath.png',
+    5: 'assets/images/office_supplies.png',
+    6: 'assets/images/war_peace.png',
+    7: 'assets/images/folded_laundry.png',
+    8: 'assets/images/v_bucks.png',
+    9: 'assets/images/goat_haul.png',
+    10: 'assets/images/broken_swing.png',
+    11: 'assets/images/painting_fence.png',
+    12: 'assets/images/re_landscape.png',
+    13: 'assets/images/temu_tiktok_shrek.png',
+    14: 'assets/images/airport.png',
+};
+
+
 //  Populate Select Element options and add Todos of users on click
 let populateAllUsers = () => {
 
@@ -28,26 +46,38 @@ let populateAllUsers = () => {
         selectEl.onchange = () => {
             divContainer.innerHTML='';
             let selectedUser = selectEl.value;
-            console.log(selectedUser)
+            
             fetch(`${apiTodoIdLink}/${selectedUser}`)
             .then((res)=>res.json())
             .then((allTaskDetails)=>{
-                console.log(allTaskDetails);
+                // if there is no task then display error message
+                if(allTaskDetails.length === 0){
+                    divContainer.innerHTML = '<p class="fs-2 fw-bold text-center">No Task(s) Here</p>'
+                }
                 
                 allTaskDetails.forEach((taskDetail)=> {
+                    console.log(taskDetail)
+                    console.log(taskDetail.id)
                     let task = document.createElement('div');
-                    console.log(task)
+                    task.classList.add('col-md-4', 'col-lg-3')
                     
-                    task.innerHTML = `
-                        
-                            <h3>Task: ${taskDetail.description}</h3>
-                            <h3>Category: ${taskDetail.category}</h3>
-                            <h3>Priority: ${taskDetail.priority}</h3>
-                            <h3>Deadline by: ${taskDetail.deadline}</h3>
-                            <h3>Completed Status: ${taskDetail.completed}</h3>
-                    
+                        task.innerHTML = `
+                    <div class='card h-100'>
+                        <img src='${userImages[taskDetail.id]}' class="card-img-top" alt='image #${taskDetail.id}'>
+                        <div class="card-body" >
+                            <h3 class="card-title">Task: ${taskDetail.description}</h3>
+                            <p class="card-text">Category: ${taskDetail.category}</p>
+                            <p class="card-text">Priority: ${taskDetail.priority}</p>
+                            <p class="card-text">Deadline by: <span class="fw-bold">${taskDetail.deadline}</span></p>
+                            <p class="card-text">Completed Status: ${taskDetail.completed}</p>
+                        </div>
+                    </div>
                     `;
+                    
                     divContainer.appendChild(task);
+                    
+                    
+                    
                     
                 })
                 
