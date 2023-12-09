6257fab9b5f04cb85df3e0b53fce476437360a4c
Javier Ballester <Javirb26@gmail.com>
2023-12-09 12:24:08 -0500

    added script to populate container with todo tasks when selecting user from dropdown

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,21 +18,48 @@ window.onload = () => {
  // grab div container and select el
  const divContainer = document.getElementById('todo-container');
  const selectEl = document.getElementById('user-select');
- const apiLink = 'http://localhost:8083/api/users';
+ const apiUserLink = 'http://localhost:8083/api/users';
+ const apiTodoIdLink = 'http://localhost:8083/api/todos/byuser'
 
 //  Populate Select Element options and add Todos of users on click
 let populateAllUsers = () => {
-    
-    selectEl.onchange = () => {
 
+        // When you select an option display the selected users todo tasks
+        selectEl.onchange = () => {
+            let selectedUser = selectEl.value;
+            console.log(selectedUser)
+            fetch(`${apiTodoIdLink}/${selectedUser}`)
+            .then((res)=>res.json())
+            .then((allTaskDetails)=>{
+                console.log(allTaskDetails);
+                
+                allTaskDetails.forEach((taskDetail)=> {
+                    let task = document.createElement('div');
+                    
+                    console.log(taskDetail.description);
+                    task.innerHTML = `
+                        <div>
+                            <h3>Task: ${taskDetail.description}</h3>
+                            <h3>Category: ${taskDetail.category}</h3>
+                            <h3>Priority: ${taskDetail.priority}</h3>
+                            <h3>Deadline by: ${taskDetail.deadline}</h3>
+                            <h3>Completed Status: ${taskDetail.completed}</h3>
+                        </div>
+                    `;
+                    divContainer.appendChild(task);
+                    
+                })
+                
+            })
+    
     }
 
     // Fetch(GET) all Users
-    fetch(apiLink)
+    fetch(apiUserLink)
         .then((res) => res.json())
         .then((allUserDetails) => {
             // store all users during session and stringify JSON to be usable
-            sessionStorage.userDetailList = JSON.stringify(allUserDetails)
+            sessionStorage.userNameList = JSON.stringify(allUserDetails)
             // call loadUserSelect to populate Select Element
             loadUserSelect(allUserDetails);
         }) .catch((err) => console.error(err))
@@ -47,6 +74,8 @@ let loadUserSelect = (arrayOfUsers) => {
     defaultOption.textContent = 'Select User';
     defaultOption.selected = true;
     selectEl.appendChild(defaultOption);
+
+    
     
     // loop through array of users and append user names to select element
     arrayOfUsers.forEach((userDetail)=> {
@@ -54,6 +83,10 @@ let loadUserSelect = (arrayOfUsers) => {
         option.value = userDetail.id;
         option.textContent = userDetail.name;
         selectEl.appendChild(option);
+        
     })
+
+    
     
 }
+
